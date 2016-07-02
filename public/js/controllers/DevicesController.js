72897e3dac1109d86b58b16706196581b5460a81
Andre Luiz Pires Silva <andre.luiz.pires.silva@gmail.com>
2016-07-02 19:59:32 -0300

    Consumindo REST com

--- public/js/controllers/DevicesController.js
+++ public/js/controllers/DevicesController.js
@@ -1,23 +1,20 @@
 angular.module('iot').controller('DevicesController',
-  function($scope, $http) {
+  function($scope, $resource) {
 
   $scope.filtro = '';
   $scope.devices = [];
+  var Device = $resource('/devices/:id');
 
-/*
-  $scope.devices = [
-    {_id: 1, ip: '192.168.1.1', name: 'Device 1'},
-    {_id: 2, ip: '192.168.1.2', name: 'Device 2'},
-    {_id: 3, ip: '192.168.1.3', name: 'Device 3'}
-  ];
-  */
-
-  $http.get('/devices')
-  .success(function(data, status, headers, config) {
-    $scope.devices = data;
-  })
-  .error(function(statusText) {
-    console.log("Não foi possível obter a lista de Equipamentos");
-    console.log(statusText);
-  });
+  function getDevices() {
+    Device.query(
+      function(devices) {
+        $scope.devices = devices;
+      },
+      function(erro) {
+        console.log("Não foi possível obter a lista de equipamentos");
+        console.log(erro);
+      }
+    );
+  }
+  getDevices();
 });
